e31d5bb08fec2be5d4841f7bbd738e3fd386efed
caioaugustoribeiro <kaio567@yahoo.com.br>
2023-06-26 14:19:03 -0300

    Ajuste

--- src/main/java/org/br/mineracao/scheduler/QuotationSheduler.java
+++ src/main/java/org/br/mineracao/scheduler/QuotationSheduler.java
@@ -5,9 +5,13 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import org.br.mineracao.service.QuotationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class QuotationSheduler {
+
+    private final Logger LOG = LoggerFactory.getLogger(QuotationSheduler.class);
 
     @Inject
     QuotationService quotationService;
@@ -15,6 +19,7 @@ public class QuotationSheduler {
     @Transactional
     @Scheduled(every = "35s", identity = "task-job")
     public  void schedule(){
+        LOG.info("-- Executabdo sheduler agora --");
         quotationService.getCurrencyPrice();
     }
 
